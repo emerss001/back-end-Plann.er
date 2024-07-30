752fe554b5852990cc8ba42cfd2d2483f56c7186
emerss001 <emersonnevess80@gmail.com>
2024-07-30 12:11:38 -0300

    adicionando dado na query de participantes

--- src/routes/participants/get-participants.ts
+++ src/routes/participants/get-participants.ts
@@ -27,6 +27,7 @@ export async function getParticipants(app: FastifyInstance) {
               name: true,
               email: true,
               is_confirmed: true,
+              is_owner: true,
             },
             orderBy: {
               name: "desc",
