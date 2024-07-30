5343fd4a5d70549ceb842837e96a42e3ca881cef
emerss001 <emersonnevess80@gmail.com>
2024-07-30 15:08:22 -0300

    removendo participantes

--- src/routes/participants/remove-participants.ts
+++ src/routes/participants/remove-participants.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { parentPort } from "worker_threads";
 
 export async function removeParticipant(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().delete(
@@ -9,16 +10,19 @@ export async function removeParticipant(app: FastifyInstance) {
     {
       schema: {
         body: z.object({
-          participantsId: z.string().uuid(),
+          participantsId: z.array(z.string().uuid()),
         }),
       },
     },
     async (request, reply) => {
       const { participantsId } = request.body;
+      const participantsRemoved = participantsId.map((id) => ({ id }));
 
       await prisma.participant.deleteMany({
         where: {
-          id: participantsId,
+          id: {
+            in: participantsId,
+          },
         },
       });
 
